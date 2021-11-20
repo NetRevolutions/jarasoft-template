2367f1189c799784e54e1452cb18b3d6dc7a3d82
netrevolutions <j_rodriguez_arruela@hotmail.com>
2021-11-20 01:17:05 -0500

    Update project, include core modules (first version)

---
+++ src/app/core/transloco/transloco.module.ts
@@ -0,0 +1,53 @@
+import { Translation, TRANSLOCO_CONFIG, TRANSLOCO_LOADER, translocoConfig, TranslocoModule, TranslocoService } from '@ngneat/transloco';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { TranslocoHttpLoader } from 'src/app/core/transloco/transloco.http-loader';
+
+
+
+
+
+@NgModule({
+  exports: [
+    TranslocoModule
+  ],
+  providers: [
+    {
+      // Provide the default Transloco configuration
+      provide: TRANSLOCO_CONFIG,
+      useValue: translocoConfig({
+        availableLangs  :[
+          {
+            id      : 'en',
+            label   : 'English' 
+          },
+          {
+            id      : 'es',
+            label   : 'Spanish'
+          }
+        ],
+        defaultLang         : 'es',
+        fallbackLang        : 'es',
+        reRenderOnLangChange: true,
+        prodMode            : environment.production
+      })
+    },
+    {
+      // Provide the default Transloco loader
+      provide  : TRANSLOCO_LOADER,
+      useClass : TranslocoHttpLoader
+    },
+    {
+      // Preload the default language before the app starts to prevent empty/jumping content
+      provide    : APP_INITIALIZER,
+      deps       : [TranslocoService],
+      useFactory : (translocoService: TranslocoService): any => (): Promise<Translation> => {
+        const defaultLang = translocoService.getActiveLang();
+        translocoService.setActiveLang(defaultLang);
+        return translocoService.load(defaultLang).toPromise()
+      }, 
+      multi      : true
+    }
+  ]
+})
+export class TranslocoCoreModule { }
