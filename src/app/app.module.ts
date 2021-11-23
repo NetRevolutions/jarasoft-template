dc2f7693290dbd51475d27a73e20a51101f57167
netrevolutions <j_rodriguez_arruela@hotmail.com>
2021-11-23 01:45:08 -0500

    Update Jarasoft-Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ExtraOptions, PreloadAllModules, RouterModule } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { appRoutes } from './app.routing';
-import { LayoutModule } from './layout/layout.module';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SettingsSidebarComponent } from './shared/settings-sidebar/settings-sidebar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 const routerConfig: ExtraOptions = {
   preloadingStrategy       : PreloadAllModules,
@@ -14,14 +27,24 @@ const routerConfig: ExtraOptions = {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NoPageFoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    SettingsSidebarComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes, routerConfig),
-
-    LayoutModule,
+    AppRoutingModule,
     MarkdownModule.forRoot({})
 
   ],
---
+++ src/app/shared/settings-sidebar/settings-sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-sidebar',
+  templateUrl: './settings-sidebar.component.html',
+  styles: [
+  ]
+})
+export class SettingsSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
