14e1d02d74c596c91438d16e68e438c5089473f2
netrevolutions <j_rodriguez_arruela@hotmail.com>
2021-12-06 01:56:25 -0500

    Eliminando cambios anteriores

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styles: [
-  ]
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  onRefreshed: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
-  ngOnInit(): void {
+
+  constructor(
+    private _changeDetectorRef: ChangeDetectorRef,
+    
+  ) { }
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  // -----------------------------------------------------------------------------------------------------
+
+  ngOnChanges(changes: SimpleChanges): void
+  {
+
+  }
+
+  ngOnInit(): void {    
+  }
+
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Public methods
+  // -----------------------------------------------------------------------------------------------------
+
+  refresh(): void 
+  {
+    // Mark for check
+    this._changeDetectorRef.markForCheck();
+
+    // Execute the observable
+    this.onRefreshed.next(true);
+
+    // Update Navigation (TODO: Por evaluar)
   }
 
 }
